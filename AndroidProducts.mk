1b185195f29ff7d3626b6b0f277cf2ce034015b7
Pritish Joshi <rjpritish@gmail.com>
2020-07-30 19:47:12 +0000

    los to pe

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_garlic.mk
+    $(LOCAL_DIR)/aosp_garlic.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_garlic-eng \
-    lineage_garlic-user \
-    lineage_garlic-userdebug
+    aosp_garlic-eng \
+    aosp_garlic-user \
+    aosp_garlic-userdebug
